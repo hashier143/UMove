faa8d24bc2509c8379583352ce5fe3030afe39fc
Fitch Ernest De Vota <fitchpogi2133@gmail.com>
2024-12-01 19:14:09 +0800

    starting of the web

---
+++ homepage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/homepage.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class = "nav-container">
+        <div>
+            <img src="styles/images/profile-icon.png">
+            <p>Log-in/Sign-up</p>
+        </div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Lessons</a></li>
+            <li><a href="">Calculator</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+    </div>
+
+    <div class="image-top">
+        <h2>UMOVE!</h2>
+        <h3>Track Your Health, Herons!</h3>
+    </div>
+
+    <div class="desc-container">
+
+        <div class="desc1">
+           <div class="container">
+                
+           </div>
+        </div>
+
+        <div class="desc2">
+          
+        </div>
+
+    </div>
+</body>
+</html>
