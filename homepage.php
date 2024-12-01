6ab6cfd1ea8f5e0e82739ec2f84d712b2f60cad4
Fitch Ernest De Vota <fitchpogi2133@gmail.com>
2024-12-01 21:56:49 +0800

    12/1/2024 10pm

--- homepage.php
+++ homepage.php
@@ -14,7 +14,7 @@
         </div>
         <ul>
             <li><a href="">Home</a></li>
-            <li><a href="">Lessons</a></li>
+            <li><a href="https://youtu.be/vahBUitgPNE?si=vhbYNoCde-VrH_pc&t=63">Lessons</a></li>
             <li><a href="">Calculator</a></li>
             <li><a href="">About</a></li>
         </ul>
@@ -39,7 +39,16 @@
         </div>
 
         <div class="desc2">
-          
+
+          <div class="desc2-1">
+            <h3>Progress Tracking Made Easy</h3>
+            <h4>Students can monitor their performance and <br>teachers can track progress, ensuring everyone stays<br> on top of their health goals.</h4>
+          </div>
+
+          <div class="desc2-2">
+w
+          </div>
+
         </div>
 
     </div>
--- landing.php
+++ landing.php
@@ -11,6 +11,15 @@
         <h1>UMOVE</h1>
         <h2>Heron's Fitness Hub</h2>
     </div>
-    <h3>Click to start</h3>
+    <h3 id="start-btn" onclick="startAnimation()">Click to Start</h3>
+    <script>
+        function startAnimation() {
+            const body = document.body;
+            body.classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = "homepage.php";
+            }, 1000);
+        }
+    </script>
 </body>
 </html>
